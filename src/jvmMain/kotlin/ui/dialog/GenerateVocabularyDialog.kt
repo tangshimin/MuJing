c55440105625d924dd12c47d2970a5b41a226eb1
shimin tang <tang_shimin@qq.com>
2023-01-18 21:30:30 +0800

    修复生成的词库后，有的字幕空格丢失，导致两个单词组合成了一个单词。

--- src/jvmMain/kotlin/ui/dialog/GenerateVocabularyDialog.kt
+++ src/jvmMain/kotlin/ui/dialog/GenerateVocabularyDialog.kt
@@ -2937,7 +2937,7 @@ fun replaceSpecialCharacter(captionContent: String): String {
         content = content.replace("</i>", "")
     }
     if (content.contains("<br />")) {
-        content = content.replace("<br />", "")
+        content = content.replace("<br />", " ")
     }
     content = removeLocationInfo(content)
     return content
