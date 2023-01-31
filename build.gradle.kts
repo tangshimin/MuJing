f08241bb49bf0bfc869a69a03b0e538ebae93fba
shimin tang <tang_shimin@qq.com>
2023-02-01 02:49:05 +0800

    升级 Compose 到1.3.0，kotlin 到1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("plugin.serialization") version "1.8.0"
 }
 
 group = "com.movcontext"
