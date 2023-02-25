c60a4f1346e54eb81f564d88c7f93aeb09541a24
shimin tang <tang_shimin@qq.com>
2023-02-26 00:24:00 +0800

    更新版本号到2.0.3。

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.movcontext"
-version = "2.0.2"
+version = "2.0.3"
 
 repositories {
     google()
--- src/jvmMain/kotlin/ui/App.kt
+++ src/jvmMain/kotlin/ui/App.kt
@@ -49,7 +49,7 @@ import kotlin.concurrent.schedule
 
 
 // build.gradle.kts 的版本也需要更改
-const val version = "v2.0.2"
+const val version = "v2.0.3"
 
 
 @ExperimentalFoundationApi
