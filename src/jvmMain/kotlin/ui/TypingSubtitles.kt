f20664e05bbc9f3233751dc51bbb5984bb41a141
shimin tang <tang_shimin@qq.com>
2023-05-03 16:37:54 +0800

    给播放多行字幕增加快捷键提示。

--- src/jvmMain/kotlin/ui/TypingSubtitles.kt
+++ src/jvmMain/kotlin/ui/TypingSubtitles.kt
@@ -895,25 +895,52 @@ fun TypingSubtitles(
 
                                         }
                                     }
-
-
-                                    Text(
-                                        modifier = Modifier.clickable {
-                                            enableMultipleLines()
-                                        },
-                                        text = buildAnnotatedString {
-                                            withStyle(
-                                                style = SpanStyle(
-                                                    color = indexColor,
-                                                    fontSize = MaterialTheme.typography.h5.fontSize,
-                                                    letterSpacing = MaterialTheme.typography.h5.letterSpacing,
-                                                    fontFamily = MaterialTheme.typography.h5.fontFamily,
-                                                )
+                                    val ctrl = LocalCtrl.current
+                                    TooltipArea(
+                                        tooltip = {
+                                            Surface(
+                                                elevation = 4.dp,
+                                                border = BorderStroke(
+                                                    1.dp,
+                                                    MaterialTheme.colors.onSurface.copy(alpha = 0.12f)
+                                                ),
+                                                shape = RectangleShape
                                             ) {
-                                                append("${index+1}")
+                                                Row(modifier = Modifier.padding(10.dp)){
+                                                    Text(text = "选择多行字幕" )
+                                                    CompositionLocalProvider(LocalContentAlpha provides 0.5f) {
+                                                        Text(text = " $ctrl + N ")
+                                                    }
+                                                }
+
                                             }
                                         },
-                                    )
+                                        delayMillis = 300,
+                                        tooltipPlacement = TooltipPlacement.ComponentRect(
+                                            anchor = Alignment.TopCenter,
+                                            alignment = Alignment.TopCenter,
+                                            offset = DpOffset.Zero
+                                        )
+                                    ) {
+                                        Text(
+                                            modifier = Modifier.clickable {
+                                                enableMultipleLines()
+                                            },
+                                            text = buildAnnotatedString {
+                                                withStyle(
+                                                    style = SpanStyle(
+                                                        color = indexColor,
+                                                        fontSize = MaterialTheme.typography.h5.fontSize,
+                                                        letterSpacing = MaterialTheme.typography.h5.letterSpacing,
+                                                        fontFamily = MaterialTheme.typography.h5.fontFamily,
+                                                    )
+                                                ) {
+                                                    append("${index+1}")
+                                                }
+                                            },
+                                        )
+                                    }
+
                                 }
 
                                 Spacer(Modifier.width(20.dp))
