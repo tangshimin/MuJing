176d3a1a052d19d0417d018146b8d5ada6725485
shimin tang <tang_shimin@qq.com>
2023-06-05 14:46:25 +0800

    删除没有被使用的代码

--- src/jvmMain/kotlin/theme/colors.kt
+++ src/jvmMain/kotlin/theme/colors.kt
@@ -6,7 +6,6 @@ import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
 
-val green = Color(0xFF09af00)// Color(46, 125, 50)
 val IDEADarkThemeOnBackground = Color(133, 144, 151)
 fun createColors(
     isDarkTheme: Boolean,
