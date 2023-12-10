ba3ff58fb53625c74381e8454934394ef33f620e
shimin tang <tang_shimin@qq.com>
2023-12-10 21:27:18 +0800

    增加已链接的视频或字幕列表

--- src/jvmMain/kotlin/ui/edit/VocabularyInfo.kt
+++ src/jvmMain/kotlin/ui/edit/VocabularyInfo.kt
@@ -1,37 +1,46 @@
 package ui.edit
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposePanel
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.rememberDialogState
 import com.formdev.flatlaf.extras.FlatSVGUtils
+import data.ExternalCaption
 import data.MutableVocabulary
 import data.Vocabulary
 import data.VocabularyType
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import player.isWindows
 import state.getResourcesFile
+import ui.dialog.ConfirmDialog
 import java.awt.BorderLayout
 import java.awt.Dimension
 import java.awt.event.WindowAdapter
 import java.awt.event.WindowEvent
 import java.awt.event.WindowFocusListener
+import java.io.File
 import javax.swing.JFrame
 
 fun vocabularyInfoWindow(
@@ -42,7 +51,7 @@ fun vocabularyInfoWindow(
     colors: Colors,
 ) {
     val window = JFrame("基本信息")
-    window.size = Dimension(650, 300)
+    window.size = Dimension(650, 520)
     val iconFile = getResourcesFile("logo/logo.svg")
     val iconImages = FlatSVGUtils.createWindowIconImages(iconFile.toURI().toURL())
     window.iconImages = iconImages
@@ -83,7 +92,7 @@ fun VocabularyInfo(
     colors: Colors
 ){
     val windowState = rememberDialogState(
-        size = DpSize(650.dp, 300.dp),
+        size = DpSize(650.dp, 520.dp),
         position = WindowPosition(parentWindow.location.x.dp,parentWindow.location.y.dp)
     )
 
@@ -102,6 +111,7 @@ fun VocabularyInfo(
                     verticalArrangement = Arrangement.Center,
                     modifier = Modifier.fillMaxSize()){
                     var videoPath by remember{ mutableStateOf(TextFieldValue(vocabulary.relateVideoPath))}
+                    var activeSave by remember{ mutableStateOf(false)}
 
                         Row(
                             verticalAlignment = Alignment.CenterVertically,
@@ -146,7 +156,13 @@ fun VocabularyInfo(
                         )
                     }
 
-
+                    // 如果是文档词库，可能会链接字幕词库。
+                    if(vocabulary.type == VocabularyType.DOCUMENT){
+                        LinkedFile(
+                            vocabulary = vocabulary,
+                            activeSave = { activeSave = true },
+                        )
+                    }
 
                     if(vocabulary.type == VocabularyType.SUBTITLES){
                         Row(
@@ -180,14 +196,22 @@ fun VocabularyInfo(
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.Center,
                         modifier = Modifier.fillMaxWidth()){
-                        if(videoPath.text != vocabulary.relateVideoPath){
-                            OutlinedButton(onClick = {
+                        LaunchedEffect(videoPath.text){
+                            activeSave = videoPath.text != vocabulary.relateVideoPath
+                        }
+                        OutlinedButton(
+                            enabled = activeSave,
+                            onClick = {
                                 vocabulary.relateVideoPath = videoPath.text
                                 saveVideoPath(vocabulary.relateVideoPath)
                                 close()
                             }){
-                                Text("保存")
-                            }
+                            Text("保存")
+                        }
+                        Spacer(Modifier.width(10.dp))
+                        OutlinedButton(
+                            onClick = {close()}){
+                            Text("关闭")
                         }
                     }
 
@@ -221,6 +245,165 @@ fun VocabularyInfo(
 
 
 
+            }
+
+        }
+
+    }
+}
+
+
+/**
+ * activeSave 是一个回调函数，删除里一个字幕词库链接后，激活保存按钮
+ */
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun LinkedFile(
+    vocabulary: MutableVocabulary,
+    activeSave: () -> Unit
+){
+    // key : 代表的是一个文件，用来批量删除和这个文件链接的所有字幕。可以是视频地址或字幕名称，有字幕名称是因为使用字幕生成的词库可以没有对应的视频。
+    // value: 和当前词库链接的字幕数量
+    val externalNameMap = remember { mutableStateMapOf<String,Int>() }
+//    val externalNameMap = mutableMapOf<String, Int>()
+    var deleted by remember{ mutableStateOf(false)}
+
+    LaunchedEffect(Unit){
+        vocabulary.wordList.forEach { word ->
+            word.externalCaptions.forEach { externalCaption ->
+
+                // 视频词库,可以播放字幕对应的视频。
+                if (externalCaption.relateVideoPath.isNotEmpty()) {
+                    var counter = externalNameMap[externalCaption.relateVideoPath]
+                    if (counter == null) {
+                        externalNameMap[externalCaption.relateVideoPath] = 1
+                    } else {
+                        counter++
+                        externalNameMap[externalCaption.relateVideoPath] = counter
+                    }
+                    // 字幕词库，使用字幕生成的词库，可以没有对应的视频，所以使用字幕的名称。
+                } else if (externalCaption.subtitlesName.isNotEmpty()) {
+                    var counter = externalNameMap[externalCaption.subtitlesName]
+                    if (counter == null) {
+                        externalNameMap[externalCaption.subtitlesName] = 1
+                    } else {
+                        counter++
+                        externalNameMap[externalCaption.subtitlesName] = counter
+                    }
+                }
+
+            }
+        }
+
+    }
+    LaunchedEffect(deleted){
+        if(deleted){
+            externalNameMap.clear()
+            vocabulary.wordList.forEach { word ->
+                word.externalCaptions.forEach { externalCaption ->
+
+                    // 视频词库,可以播放字幕对应的视频。
+                    if (externalCaption.relateVideoPath.isNotEmpty()) {
+                        var counter = externalNameMap[externalCaption.relateVideoPath]
+                        if (counter == null) {
+                            externalNameMap[externalCaption.relateVideoPath] = 1
+                        } else {
+                            counter++
+                            externalNameMap[externalCaption.relateVideoPath] = counter
+                        }
+                        // 字幕词库，使用字幕生成的词库，可以没有对应的视频，所以使用字幕的名称。
+                    } else if (externalCaption.subtitlesName.isNotEmpty()) {
+                        var counter = externalNameMap[externalCaption.subtitlesName]
+                        if (counter == null) {
+                            externalNameMap[externalCaption.subtitlesName] = 1
+                        } else {
+                            counter++
+                            externalNameMap[externalCaption.subtitlesName] = counter
+                        }
+                    }
+
+                }
+            }
+            deleted = false
+        }
+
+    }
+    Column(Modifier.width(IntrinsicSize.Max).padding(start = 10.dp,bottom = 20.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp)
+        ) { Text("已链接的视频或字幕") }
+        if (externalNameMap.isNotEmpty()) {
+            val boxHeight by remember(externalNameMap.size){
+                    derivedStateOf {
+                        val size = externalNameMap.size
+                        if(size <=5){
+                            externalNameMap.size * 48.dp
+                        }else{
+                            240.dp
+                        }
+                    }
+            }
+            Box(Modifier
+                .height(boxHeight)
+                .fillMaxWidth()
+                .border(border = BorderStroke(1.dp, MaterialTheme.colors.onSurface.copy(alpha = 0.12f)))
+            ){
+                val stateVertical = rememberScrollState(0)
+                val scrollbarStyle = LocalScrollbarStyle.current.copy(shape = if(isWindows()) RectangleShape else RoundedCornerShape(4.dp))
+                Column (Modifier
+                    .padding(start = 20.dp)
+                    .verticalScroll(stateVertical)
+                    ){
+                    externalNameMap.forEach { (path, count) ->
+                        var showConfirmationDialog by remember { mutableStateOf(false) }
+                        Row(verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.clickable{}) {
+                            val name = File(path).nameWithoutExtension
+                            if (showConfirmationDialog) {
+                                ConfirmDialog(
+                                    message = "确定要删除 $name 的所有字幕吗?",
+                                    confirm = {
+                                        vocabulary.wordList.forEach { word ->
+                                            val tempList = mutableListOf<ExternalCaption>()
+                                            word.externalCaptions.forEach { externalCaption ->
+                                                if (externalCaption.relateVideoPath == path || externalCaption.subtitlesName == path) {
+                                                    tempList.add(externalCaption)
+                                                }
+                                            }
+                                            word.externalCaptions.removeAll(tempList)
+                                        }
+                                        deleted = true
+                                        showConfirmationDialog = false
+                                        activeSave()
+                                    },
+                                    close = { showConfirmationDialog = false }
+                                )
+                            }
+                            Text(
+                                text = name,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.width(450.dp).padding(start = 10.dp,end = 10.dp)
+                            )
+                            Text("$count", modifier = Modifier.width(60.dp))
+                            IconButton(onClick = { showConfirmationDialog = true },modifier = Modifier.padding(end = 10.dp)) {
+                                Icon(
+                                    imageVector = Icons.Filled.Delete,
+                                    contentDescription = "",
+                                    tint = MaterialTheme.colors.onBackground
+                                )
+                            }
+
+
+                        }
+                    }
+                }
+                VerticalScrollbar(
+                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+                    adapter = rememberScrollbarAdapter(stateVertical),
+                    style = scrollbarStyle,
+                )
             }
 
         }
