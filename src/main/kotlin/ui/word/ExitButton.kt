25ba426fcc170b8c3ca79fbbdd11b61d068cc103
shimin tang <tang_shimin@qq.com>
2024-08-07 23:15:05 +0800

    重构：提取 ExitButton

---
+++ src/main/kotlin/ui/word/ExitButton.kt
@@ -0,0 +1,52 @@
+package ui.word
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.TooltipArea
+import androidx.compose.foundation.TooltipPlacement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+
+/** 退出按钮 */
+@Composable
+@OptIn(ExperimentalFoundationApi::class)
+fun ExitButton(
+    tooltip: String,
+    onClick: () -> Unit
+){
+    TooltipArea(
+        tooltip = {
+            Surface(
+                elevation = 4.dp,
+                border = BorderStroke(1.dp, MaterialTheme.colors.onSurface.copy(alpha = 0.12f)),
+                shape = RectangleShape
+            ) {
+                Text(text = tooltip, modifier = Modifier.padding(10.dp))
+            }
+        },
+        delayMillis = 300,
+        tooltipPlacement = TooltipPlacement.ComponentRect(
+            anchor = Alignment.BottomCenter,
+            alignment = Alignment.BottomCenter,
+            offset = DpOffset.Zero
+        )
+    ) {
+        IconButton(onClick = {
+            onClick()
+        }) {
+            Icon(
+                Icons.Filled.Close,
+                contentDescription = "Localized description",
+                tint = MaterialTheme.colors.primary
+            )
+        }
+    }
+}
--- src/main/kotlin/ui/word/WordScreen.kt
+++ src/main/kotlin/ui/word/WordScreen.kt
@@ -2828,42 +2828,6 @@ fun CopyButton(wordValue:String){
     }
 }
 
-/** 退出按钮*/
-@Composable
-@OptIn(ExperimentalFoundationApi::class)
-fun ExitButton(
-    tooltip: String,
-    onClick: () -> Unit
-){
-    TooltipArea(
-        tooltip = {
-            Surface(
-                elevation = 4.dp,
-                border = BorderStroke(1.dp, MaterialTheme.colors.onSurface.copy(alpha = 0.12f)),
-                shape = RectangleShape
-            ) {
-                Text(text = tooltip, modifier = Modifier.padding(10.dp))
-            }
-        },
-        delayMillis = 300,
-        tooltipPlacement = TooltipPlacement.ComponentRect(
-            anchor = Alignment.BottomCenter,
-            alignment = Alignment.BottomCenter,
-            offset = DpOffset.Zero
-        )
-    ) {
-        IconButton(onClick = {
-            onClick()
-        }) {
-            Icon(
-                Icons.Filled.Close,
-                contentDescription = "Localized description",
-                tint = MaterialTheme.colors.primary
-            )
-        }
-    }
-}
-
 
 @OptIn(ExperimentalSerializationApi::class)
 @Composable
