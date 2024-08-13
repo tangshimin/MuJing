2520b1da12791c43cdafbc4af125fdff2b1359ca
shimin tang <tang_shimin@qq.com>
2024-08-14 00:15:11 +0800

    添加 logback-classic 依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation ("io.ktor:ktor-client-cio:2.3.11")
     implementation ("net.java.dev.jna:jna:5.14.0")
     implementation ("net.java.dev.jna:jna-platform:5.14.0")
+    implementation ("ch.qos.logback:logback-classic:1.4.14")
 
     testImplementation(compose.desktop.uiTestJUnit4)
 }
