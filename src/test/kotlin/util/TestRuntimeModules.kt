6e4b3a866bef98d82d52bfaf19f34d70467cb8d2
shimin tang <tang_shimin@qq.com>
2024-09-06 15:53:21 +0800

    添加 TestRuntimeModules.kt

---
+++ src/test/kotlin/util/TestRuntimeModules.kt
@@ -0,0 +1,36 @@
+package util
+
+import org.junit.Test
+import player.isWindows
+import java.io.File
+
+/**
+ * 有时候增加了一些代码，本地运行没有问题，但是打包后运行时会出现一些问题，比如找不到类，这时候可能是因为没有包含某些模块。
+ * 这个测试用例会输出需要包含的模块, 如果模块不匹配，就修改 `build.gradle.kts` 中的 `nativeDistributions`
+ */
+class TestRuntimeModules {
+
+    @Test
+    fun `Test Native Distribution Modules`() {
+        val workDir = File("").absolutePath
+        val task = "gradlew suggestRuntimeModules"
+        val command = mutableListOf<String>()
+        if(isWindows()){
+            command.add("cmd")
+            command.add("/c")
+        }
+        command.add(task)
+
+        val processBuilder = ProcessBuilder(command)
+        processBuilder.directory(File(workDir))
+        val process = processBuilder.start()
+        val output = process.inputStream.bufferedReader().readText()
+        process.waitFor()
+
+        assert(output.contains("Suggested runtime modules to include:"))
+        val expectModules = "modules(\"java.compiler\", \"java.instrument\", \"java.management\", \"java.prefs\", \"java.security.jgss\", \"java.sql\", \"java.xml.crypto\", \"jdk.unsupported\")"
+        assert(output.contains(expectModules)) { "Expect $expectModules in output" }
+        println("Output:\n$output")
+    }
+}
+
