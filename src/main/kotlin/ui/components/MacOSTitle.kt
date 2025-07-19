e1e524b716b7ff8d64683a1ca888444e67e4f6c1
tang shimin <tang_shimin@qq.com>
2025-07-19 13:31:52 +0800

    添加 MacOS 窗口按钮间距设置以支持主题变化

--- src/main/kotlin/ui/components/MacOSTitle.kt
+++ src/main/kotlin/ui/components/MacOSTitle.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposeWindow
+import com.formdev.flatlaf.FlatClientProperties
 
 
 @Composable
@@ -24,6 +25,13 @@ fun MacOSTitle(
         window.rootPane.putClientProperty("apple.awt.fullWindowContent", true)
         window.rootPane.putClientProperty("apple.awt.transparentTitleBar", true)
         window.rootPane.putClientProperty("apple.awt.windowTitleVisible", false)
+//        System.setProperty("apple.awt.application.appearance", "system")
+        //加了这一行，全屏的时候工具栏的背景颜色才会跟随主题一起变化
+        window.rootPane.putClientProperty(
+            FlatClientProperties.MACOS_WINDOW_BUTTONS_SPACING,
+            FlatClientProperties.MACOS_WINDOW_BUTTONS_SPACING_LARGE
+        )
+
     }
 
 }
