307ef68961c876d2cec0e2dc96d8b744996b8842
tang shimin <tang_shimin@qq.com>
2025-07-19 11:29:29 +0800

    更新 FlatLaf 依赖至 3.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("io.github.microutils:kotlin-logging:2.1.21")
     implementation("uk.co.caprica:vlcj:4.8.2")
-    implementation("com.formdev:flatlaf:3.1")
+    implementation("com.formdev:flatlaf:3.6.1")
     implementation("com.formdev:flatlaf-extras:2.6")
     implementation("org.apache.opennlp:opennlp-tools:1.9.4")
     implementation("org.apache.pdfbox:pdfbox:2.0.24")
