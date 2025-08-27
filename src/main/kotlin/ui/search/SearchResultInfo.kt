ef8ddffe484ec35bc2728c9ee7e466f7ba946618
tang shimin <tang_shimin@qq.com>
2025-08-27 16:08:18 +0800

    新增中英文释义切换标签页

--- src/main/kotlin/ui/search/SearchResultInfo.kt
+++ src/main/kotlin/ui/search/SearchResultInfo.kt
@@ -4,14 +4,22 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import data.Word
 import kotlinx.serialization.ExperimentalSerializationApi
 import player.AudioButton
+import player.danmaku.TextBox
 import state.AppState
 import ui.wordscreen.WordScreenState
 import tts.AzureTTS
@@ -37,7 +45,7 @@ fun SearchResultInfo(
             azureTTS = azureTTS
         )
     }
-    Divider()
+
     Morphology(
         word = word,
         isPlaying = false,
@@ -47,16 +55,40 @@ fun SearchResultInfo(
     )
     Spacer(Modifier.height(8.dp))
     Divider()
-    SelectionContainer {
-        Text(text = word.definition,
-            color = MaterialTheme.colors.onBackground,
-            modifier = Modifier.padding(top = 8.dp,bottom = 8.dp))
+    var tabState by remember { mutableStateOf(0) }
+
+    TabRow(
+        selectedTabIndex = tabState,
+        backgroundColor = Color.Transparent
+    ) {
+        Tab(
+            text = { Text("中文") },
+            selected = tabState == 0,
+            onClick = { tabState = 0 }
+        )
+        Tab(
+            text = { Text("英文") },
+            selected = tabState == 1,
+            onClick = { tabState = 1 }
+        )
     }
-    Divider()
-    SelectionContainer {
-        Text(word.translation,
-            color = MaterialTheme.colors.onBackground,
-            modifier = Modifier.padding(top = 8.dp,bottom = 8.dp))
+    when (tabState) {
+        0 -> {
+            SelectionContainer {
+                Text(word.translation,
+                    color = MaterialTheme.colors.onBackground,
+                    modifier = Modifier.padding(top = 8.dp,bottom = 8.dp))
+            }
+        }
+
+        1 -> {
+            SelectionContainer {
+                Text(text = word.definition,
+                    color = MaterialTheme.colors.onBackground,
+                    modifier = Modifier.padding(top = 8.dp,bottom = 8.dp))
+            }
+        }
     }
+
     Divider()
 }
